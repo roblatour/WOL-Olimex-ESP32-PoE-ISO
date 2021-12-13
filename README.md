57d9cff3c34baa21822c70468e4756ec97055af6
Rob Latour <5200730+roblatour@users.noreply.github.com>
2021-12-13 18:00:31 -0500

    updates to add certificates
    updates to add certificates as is now required for https accesses

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # WOL-Olimex-ESP32-PoE-ISO
+
 Arduino code for an Olimex ESP32-PoE-IS to wake up your computer (WOL) triggered by a Pushbullet note
--- WOL-Olimex-ESP32-PoE-ISO.ino
+++ WOL-Olimex-ESP32-PoE-ISO.ino
@@ -1,15 +1,12 @@
 /*
-    This sketch was developed by Rob Latour Copyright (c) 2019 - 2020
+    This sketch was developed by Rob Latour Copyright (c) 2019 - 2021
 
     license: MIT
 
     usage: wakeup your computer (WOL) triggered by a Pushbullet note
 
-    Additional boards manager
-      https://dl.espressif.com/dl/package_esp32_dev_index.json
-
-    board: OLKMEX ESP32-POE
-      esp32       1.0.2   https://github.com/espressif/arduino-esp32
+    Board Manager: ESP32 v2.0.1
+    board: OLKMEX ESP32-POE-ISO
 
     with special thanks to those that developed these libraries:
       JSON        6.12     by Benoit Blachon    https://arduinojson.org/?utm_source=meta&utm_medium=library.properties
@@ -25,10 +22,6 @@
 #include <WebSocketsClient.h>
 
 #include <ETH.h>
-
-// Type of the Ethernet PHY (LAN8720 or TLK110)
-// #define ETH_TYPE        ETH_PHY_LAN8720
-// #define ETH_TYPE        ETH_PHY_TLK110
 
 #include <WiFi.h>
 #include <WiFiUdp.h>
@@ -70,7 +63,70 @@ const String PushBullet_Server_Directory = "/websocket/";
 const String PushBullet_KeepAlive_ID = "rob_wol_active";
 const int PushBullet_Server_Port = 443;
 const char* host = "api.pushbullet.com";
+const char* Pushbullet_API_root_ca = "-----BEGIN CERTIFICATE-----\n" \
+                                     "MIIFYjCCBEqgAwIBAgIQd70NbNs2+RrqIQ/E8FjTDTANBgkqhkiG9w0BAQsFADBX\n" \
+                                     "MQswCQYDVQQGEwJCRTEZMBcGA1UEChMQR2xvYmFsU2lnbiBudi1zYTEQMA4GA1UE\n" \
+                                     "CxMHUm9vdCBDQTEbMBkGA1UEAxMSR2xvYmFsU2lnbiBSb290IENBMB4XDTIwMDYx\n" \
+                                     "OTAwMDA0MloXDTI4MDEyODAwMDA0MlowRzELMAkGA1UEBhMCVVMxIjAgBgNVBAoT\n" \
+                                     "GUdvb2dsZSBUcnVzdCBTZXJ2aWNlcyBMTEMxFDASBgNVBAMTC0dUUyBSb290IFIx\n" \
+                                     "MIICIjANBgkqhkiG9w0BAQEFAAOCAg8AMIICCgKCAgEAthECix7joXebO9y/lD63\n" \
+                                     "ladAPKH9gvl9MgaCcfb2jH/76Nu8ai6Xl6OMS/kr9rH5zoQdsfnFl97vufKj6bwS\n" \
+                                     "iV6nqlKr+CMny6SxnGPb15l+8Ape62im9MZaRw1NEDPjTrETo8gYbEvs/AmQ351k\n" \
+                                     "KSUjB6G00j0uYODP0gmHu81I8E3CwnqIiru6z1kZ1q+PsAewnjHxgsHA3y6mbWwZ\n" \
+                                     "DrXYfiYaRQM9sHmklCitD38m5agI/pboPGiUU+6DOogrFZYJsuB6jC511pzrp1Zk\n" \
+                                     "j5ZPaK49l8KEj8C8QMALXL32h7M1bKwYUH+E4EzNktMg6TO8UpmvMrUpsyUqtEj5\n" \
+                                     "cuHKZPfmghCN6J3Cioj6OGaK/GP5Afl4/Xtcd/p2h/rs37EOeZVXtL0m79YB0esW\n" \
+                                     "CruOC7XFxYpVq9Os6pFLKcwZpDIlTirxZUTQAs6qzkm06p98g7BAe+dDq6dso499\n" \
+                                     "iYH6TKX/1Y7DzkvgtdizjkXPdsDtQCv9Uw+wp9U7DbGKogPeMa3Md+pvez7W35Ei\n" \
+                                     "Eua++tgy/BBjFFFy3l3WFpO9KWgz7zpm7AeKJt8T11dleCfeXkkUAKIAf5qoIbap\n" \
+                                     "sZWwpbkNFhHax2xIPEDgfg1azVY80ZcFuctL7TlLnMQ/0lUTbiSw1nH69MG6zO0b\n" \
+                                     "9f6BQdgAmD06yK56mDcYBZUCAwEAAaOCATgwggE0MA4GA1UdDwEB/wQEAwIBhjAP\n" \
+                                     "BgNVHRMBAf8EBTADAQH/MB0GA1UdDgQWBBTkrysmcRorSCeFL1JmLO/wiRNxPjAf\n" \
+                                     "BgNVHSMEGDAWgBRge2YaRQ2XyolQL30EzTSo//z9SzBgBggrBgEFBQcBAQRUMFIw\n" \
+                                     "JQYIKwYBBQUHMAGGGWh0dHA6Ly9vY3NwLnBraS5nb29nL2dzcjEwKQYIKwYBBQUH\n" \
+                                     "MAKGHWh0dHA6Ly9wa2kuZ29vZy9nc3IxL2dzcjEuY3J0MDIGA1UdHwQrMCkwJ6Al\n" \
+                                     "oCOGIWh0dHA6Ly9jcmwucGtpLmdvb2cvZ3NyMS9nc3IxLmNybDA7BgNVHSAENDAy\n" \
+                                     "MAgGBmeBDAECATAIBgZngQwBAgIwDQYLKwYBBAHWeQIFAwIwDQYLKwYBBAHWeQIF\n" \
+                                     "AwMwDQYJKoZIhvcNAQELBQADggEBADSkHrEoo9C0dhemMXoh6dFSPsjbdBZBiLg9\n" \
+                                     "NR3t5P+T4Vxfq7vqfM/b5A3Ri1fyJm9bvhdGaJQ3b2t6yMAYN/olUazsaL+yyEn9\n" \
+                                     "WprKASOshIArAoyZl+tJaox118fessmXn1hIVw41oeQa1v1vg4Fv74zPl6/AhSrw\n" \
+                                     "9U5pCZEt4Wi4wStz6dTZ/CLANx8LZh1J7QJVj2fhMtfTJr9w4z30Z209fOU0iOMy\n" \
+                                     "+qduBmpvvYuR7hZL6Dupszfnw0Skfths18dG9ZKb59UhvmaSGZRVbNQpsg3BZlvi\n" \
+                                     "d0lIKO2d1xozclOzgjXPYovJJIultzkMu34qQb9Sz/yilrbCgj8=\n" \
+                                     "-----END CERTIFICATE-----\n";
+									 
 const char* KeepAliveHost = "zebra.pushbullet.com";
+const char* Pushbullet_KeepAlive_root_ca = "-----BEGIN CERTIFICATE-----\n" \    
+                                           "MIIFFjCCAv6gAwIBAgIRAJErCErPDBinU/bWLiWnX1owDQYJKoZIhvcNAQELBQAw\n" \
+                                           "TzELMAkGA1UEBhMCVVMxKTAnBgNVBAoTIEludGVybmV0IFNlY3VyaXR5IFJlc2Vh\n" \
+                                           "cmNoIEdyb3VwMRUwEwYDVQQDEwxJU1JHIFJvb3QgWDEwHhcNMjAwOTA0MDAwMDAw\n" \
+                                           "WhcNMjUwOTE1MTYwMDAwWjAyMQswCQYDVQQGEwJVUzEWMBQGA1UEChMNTGV0J3Mg\n" \
+                                           "RW5jcnlwdDELMAkGA1UEAxMCUjMwggEiMA0GCSqGSIb3DQEBAQUAA4IBDwAwggEK\n" \
+                                           "AoIBAQC7AhUozPaglNMPEuyNVZLD+ILxmaZ6QoinXSaqtSu5xUyxr45r+XXIo9cP\n" \
+                                           "R5QUVTVXjJ6oojkZ9YI8QqlObvU7wy7bjcCwXPNZOOftz2nwWgsbvsCUJCWH+jdx\n" \
+                                           "sxPnHKzhm+/b5DtFUkWWqcFTzjTIUu61ru2P3mBw4qVUq7ZtDpelQDRrK9O8Zutm\n" \
+                                           "NHz6a4uPVymZ+DAXXbpyb/uBxa3Shlg9F8fnCbvxK/eG3MHacV3URuPMrSXBiLxg\n" \
+                                           "Z3Vms/EY96Jc5lP/Ooi2R6X/ExjqmAl3P51T+c8B5fWmcBcUr2Ok/5mzk53cU6cG\n" \
+                                           "/kiFHaFpriV1uxPMUgP17VGhi9sVAgMBAAGjggEIMIIBBDAOBgNVHQ8BAf8EBAMC\n" \
+                                           "AYYwHQYDVR0lBBYwFAYIKwYBBQUHAwIGCCsGAQUFBwMBMBIGA1UdEwEB/wQIMAYB\n" \
+                                           "Af8CAQAwHQYDVR0OBBYEFBQusxe3WFbLrlAJQOYfr52LFMLGMB8GA1UdIwQYMBaA\n" \
+                                           "FHm0WeZ7tuXkAXOACIjIGlj26ZtuMDIGCCsGAQUFBwEBBCYwJDAiBggrBgEFBQcw\n" \
+                                           "AoYWaHR0cDovL3gxLmkubGVuY3Iub3JnLzAnBgNVHR8EIDAeMBygGqAYhhZodHRw\n" \
+                                           "Oi8veDEuYy5sZW5jci5vcmcvMCIGA1UdIAQbMBkwCAYGZ4EMAQIBMA0GCysGAQQB\n" \
+                                           "gt8TAQEBMA0GCSqGSIb3DQEBCwUAA4ICAQCFyk5HPqP3hUSFvNVneLKYY611TR6W\n" \
+                                           "PTNlclQtgaDqw+34IL9fzLdwALduO/ZelN7kIJ+m74uyA+eitRY8kc607TkC53wl\n" \
+                                           "ikfmZW4/RvTZ8M6UK+5UzhK8jCdLuMGYL6KvzXGRSgi3yLgjewQtCPkIVz6D2QQz\n" \
+                                           "CkcheAmCJ8MqyJu5zlzyZMjAvnnAT45tRAxekrsu94sQ4egdRCnbWSDtY7kh+BIm\n" \
+                                           "lJNXoB1lBMEKIq4QDUOXoRgffuDghje1WrG9ML+Hbisq/yFOGwXD9RiX8F6sw6W4\n" \
+                                           "avAuvDszue5L3sz85K+EC4Y/wFVDNvZo4TYXao6Z0f+lQKc0t8DQYzk1OXVu8rp2\n" \
+                                           "yJMC6alLbBfODALZvYH7n7do1AZls4I9d1P4jnkDrQoxB3UqQ9hVl3LEKQ73xF1O\n" \
+                                           "yK5GhDDX8oVfGKF5u+decIsH4YaTw7mP3GFxJSqv3+0lUFJoi5Lc5da149p90Ids\n" \
+                                           "hCExroL1+7mryIkXPeFM5TgO9r0rvZaBFOvV2z0gp35Z0+L4WPlbuEjN/lxPFin+\n" \
+                                           "HlUjr8gRsI3qfJOQFy/9rKIJR0Y/8Omwt/8oTWgy1mdeHmmjk7j1nYsvC9JSQ6Zv\n" \
+                                           "MldlTTKB3zhThV1+XWYp6rjd5JW1zbVWEkLNxE7GJThEUG3szgBVGP7pSWTUTsqX\n" \
+                                           "nLRbwHOoq7hHwg==\n" \
+                                           "-----END CERTIFICATE-----\n";
+
 const int https_Port = 443;
 bool PushBullet_connected = false;
 String My_PushBullet_Client_ID = "";
@@ -157,19 +213,15 @@ static bool eth_connected = false;
 void WiFiEvent(WiFiEvent_t event)
 {
   switch (event) {
-
-    case SYSTEM_EVENT_ETH_START:
-      Serial.println(" ");
+    case ARDUINO_EVENT_ETH_START:
       Serial.println("ETH Started");
-      ETH.setHostname("WOL-server");
+      //set eth hostname here
+      ETH.setHostname("esp32-ethernet");
       break;
-
-    case SYSTEM_EVENT_ETH_CONNECTED:
-      Serial.println(" ");
+    case ARDUINO_EVENT_ETH_CONNECTED:
       Serial.println("ETH Connected");
       break;
-
-    case SYSTEM_EVENT_ETH_GOT_IP:
+    case ARDUINO_EVENT_ETH_GOT_IP:
       Serial.print("ETH MAC: ");
       Serial.print(ETH.macAddress());
       Serial.print(", IPv4: ");
@@ -182,17 +234,14 @@ void WiFiEvent(WiFiEvent_t event)
       Serial.println("Mbps");
       eth_connected = true;
       break;
-
-    case SYSTEM_EVENT_ETH_DISCONNECTED:
+    case ARDUINO_EVENT_ETH_DISCONNECTED:
       Serial.println("ETH Disconnected");
       eth_connected = false;
       break;
-
-    case SYSTEM_EVENT_ETH_STOP:
+    case ARDUINO_EVENT_ETH_STOP:
       Serial.println("ETH Stopped");
       eth_connected = false;
       break;
-
     default:
       break;
   }
@@ -318,7 +367,6 @@ void webSocketEvent(WStype_t type, uint8_t * payload, size_t length) {
         DynamicJsonDocument jsonDocument(4096);
         deserializeJson(jsonDocument, payload);
 
-
         if (jsonDocument["type"] == "nop") {
 
           Serial.println("nop");
@@ -332,6 +380,8 @@ void webSocketEvent(WStype_t type, uint8_t * payload, size_t length) {
         if ((jsonDocument["type"] == "tickle") && (jsonDocument["subtype"] == "push")) {
 
           WiFiClientSecure client1;
+	  client1.setCACert(Pushbullet_API_root_ca);
+		  
           if (!client1.connect(host, https_Port)) {
             Serial.println("Connection failed");
             return;
@@ -340,11 +390,11 @@ void webSocketEvent(WStype_t type, uint8_t * payload, size_t length) {
           client1.println("GET /v2/pushes?limit=1 HTTP/1.1");
           client1.println("Host: " + String(host));
           client1.println("Authorization: Bearer " + My_PushBullet_Access_Token);
-          client1.println("Content-Type : application/json");
-          client1.println("Content-Length : 0");
+          client1.println("Content-Type: application/json");
+          client1.println("Content-Length: 0");
           client1.println();
 
-          // Serial.print(" waiting for the details ");
+          // Serial.print("waiting for the details ");
           int WaitLimit = 0;
           while ((!client1.available()) && (WaitLimit < 250)) {
             delay(50);
@@ -460,6 +510,7 @@ void PushbulletDismissPush(String Push_Iden) {
   Serial.println(" dismissing push");
 
   WiFiClientSecure client;
+  client.setCACert(Pushbullet_API_root_ca);
 
   if (!client.connect(host, https_Port)) {
     Serial.println(" connection failed");
@@ -518,6 +569,8 @@ void PushbulletDismissPush(String Push_Iden) {
 void GetPushbulletClientID() {
 
   WiFiClientSecure client1;
+  client1.setCACert(Pushbullet_API_root_ca);
+  
   if (!client1.connect(host, https_Port)) {
     Serial.println("Connection failed");
     return;
@@ -526,8 +579,8 @@ void GetPushbulletClientID() {
   client1.println("GET /v2/users/me HTTP/1.1");
   client1.println("Host: " + String(host));
   client1.println("Authorization: Bearer " + My_PushBullet_Access_Token);
-  client1.println("Content-Type : application/json");
-  client1.println("Content-Length : 0");
+  client1.println("Content-Type: application/json");
+  client1.println("Content-Length: 0");
   client1.println();
 
   // Serial.print(" waiting for the details ");
@@ -569,9 +622,10 @@ void PushbulletStayAlive() {
   Serial.print("stay alive request ");
 
   WiFiClientSecure client;
+  client.setCACert(Pushbullet_KeepAlive_root_ca);
 
   if (!client.connect(KeepAliveHost, https_Port)) {
-    Serial.println(" connection failed");
+    Serial.println("connection failed");
     return;
   }
 
